5c4159d4018927d60749dc93f42c6dbddf8a093d
amajd9323-sketch <amajd9323@gmail.com>
2026-08-16 13:29:02 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='hesabi-pro-v4-linked';
+const CORE=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(a=>Promise.all(a.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET')return;
+ e.respondWith(fetch(e.request).then(r=>{caches.open(CACHE).then(c=>c.put(e.request,r.clone())).catch(()=>{});return r;}).catch(()=>caches.match(e.request)));
+});
