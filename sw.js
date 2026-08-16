39ffc91ea892828f45480aa128e568abaa654e87
amajd9323-sketch <amajd9323@gmail.com>
2026-08-16 18:11:38 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hesabi-pro-v6-scan';
+const CACHE='hesabi-pro-v7-features';
 const CORE=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(a=>Promise.all(a.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
